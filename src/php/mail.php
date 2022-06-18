fb03448a6328d3fafd1d17421a97fb9205f087d8
Asma-pixel <68337719+Asma-pixel@users.noreply.github.com>
2022-06-18 20:48:24 +0300

    Update mail.php
    change mail.php

--- src/php/mail.php
+++ src/php/mail.php
@@ -21,11 +21,51 @@ function mail_utf8($to, $from_user, $from_email,
     $number = htmlentities($_POST["number"]);
     $message ="
         <html lang='en'>
-          <body style='display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 0 auto; width: 80%;'>
-              <h1>Здравствуйте меня зовут $surname $name $patronymic</h1>
-              <p>Моя дата рождения $date , и я очень хочу работать в вашей компаниии.</p>
-              <p>Напишите мне на мою почту: $email</p>
-              <p>Вы также можете мне перезвонить по номеру: $number</p>
+          <body style='margin: 0 auto; width: 80%;'>
+              <h1>Здравствуйте меня зовут $surname $name $patronymic</h1> <br><br>
+              <p>Моя дата рождения $date , и я очень хочу работать в вашей компаниии.</p><br><br>
+              <p>Напишите мне на мою почту: $email</p><br><br>
+              <p>Вы также можете мне перезвонить по номеру: $number</p><br><br>
+          </body>
+        </html>";
+
+        $to = 'serafimcomru91@gmail.com'; 
+
+        $subject = 'Письмо с сайта по поиску менеджера';
+        
+     
+        mail_utf8($to, "arlift.com","arlift@gmail.com", $subject, $message);
+        header("Location: ../../index.html");
+        exit;
+       
+ ?><?php
+
+function mail_utf8($to, $from_user, $from_email,
+                                             $subject = '(No subject)', $message = '')
+   {
+      $from_user = "=?UTF-8?B?".base64_encode($from_user)."?=";
+      $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
+
+      $headers = "From: $from_user <$from_email>\r\n".
+               "MIME-Version: 1.0" . "\r\n" .
+               "Content-type: text/html; charset=UTF-8" . "\r\n";
+
+     return mail($to, $subject, $message, $headers);
+   }
+   
+    $name = htmlentities($_POST["name"]);
+    $surname = htmlentities($_POST["surname"]);
+    $patronymic = htmlentities($_POST["patronymic"]);
+    $date = htmlentities($_POST["date"]);
+    $email = htmlentities($_POST["email"]);
+    $number = htmlentities($_POST["number"]);
+    $message ="
+        <html lang='en'>
+          <body style='margin: 0 auto; width: 80%;'>
+              <h1>Здравствуйте меня зовут $surname $name $patronymic</h1> <br><br>
+              <p>Моя дата рождения $date , и я очень хочу работать в вашей компаниии.</p><br><br>
+              <p>Напишите мне на мою почту: $email</p><br><br>
+              <p>Вы также можете мне перезвонить по номеру: $number</p><br><br>
           </body>
         </html>";
 
